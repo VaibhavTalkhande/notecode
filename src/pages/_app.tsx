5210ff994746744c3864b164a798d10107e426e8
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-10-24 18:42:13 +0530

    project completed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
 
-import { api } from "~/utils/api";
+import { api } from "../utils/api";
 
-import "~/styles/globals.css";
+import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -15,19 +15,59 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+  import { PrismaClient } from '@prisma/client';
 
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-          createdBy: { connect: { id: ctx.session.user.id } },
-        },
+  import { z } from "zod";
+
+  import {
+    createTRPCRouter,
+    protectedProcedure,
+  } from "~/server/api/trpc";
+
+  const prisma = new PrismaClient();
+
+  export const postRouter = createTRPCRouter({
+    create: protectedProcedure
+      .input(z.object({ name: z.string().min(1) }))
+      .mutation(async ({ ctx, input }) => {
+        // simulate a slow db call
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        return prisma.post.create({
+          data: {
+            name: input.name,
+            createdBy: { connect: { id: ctx.session.user.id } },
+          },
+        });
+      }),
+
+    getLatest: protectedProcedure.query(({ ctx }) => {
+      return prisma.post.findFirst({
+        orderBy: { createdAt: "desc" },
+        where: { createdBy: { id: ctx.session.user.id } },
       });
     }),
+
+    getSecretMessage: protectedProcedure.query(() => {
+      return "you can now see this secret message!";
+    }),
+  });
+
+  export const postRouter = createTRPCRouter({
+    create: protectedProcedure
+      .input(z.object({ name: z.string().min(1) }))
+      .mutation(async ({ ctx, input }) => {
+        // simulate a slow db call
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        return prisma.post.create({
+          data: {
+            name: input.name,
+            createdBy: { connect: { id: ctx.session.user.id } },
+          },
+        });
+      }),
+  });
 
   getLatest: protectedProcedure.query(({ ctx }) => {
     return ctx.db.post.findFirst({
